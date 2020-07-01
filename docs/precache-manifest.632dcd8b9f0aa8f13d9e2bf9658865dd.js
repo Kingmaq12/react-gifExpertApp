4e42935ac093956c39bdd66b705a3364d04262ad
Cesar <cvasquez647@gmail.com>
2020-06-30 19:01:35 -0500

    Added docs folder

---
+++ docs/precache-manifest.632dcd8b9f0aa8f13d9e2bf9658865dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7126c0cb34eeb09d8ae0dc2fa22b9f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f04828ca8575bed8b8b2",
+    "url": "/static/css/main.dbd80c67.chunk.css"
+  },
+  {
+    "revision": "c3681b97ad95d11cd3c6",
+    "url": "/static/js/2.a7affb1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7affb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04828ca8575bed8b8b2",
+    "url": "/static/js/main.27f006cd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
